e3a06dedb1ce5ed2d6dd66b4291254658c3c37ef
Vasu77df <vasu3797@gmail.com>
2020-02-23 14:13:28 -0600

    removed pressure

--- ble_rx.py
+++ ble_rx.py
@@ -1,5 +1,28 @@
 from bluepy import btle
 import json
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from time import sleep
+
+#authentication information and topic name 
+rootCAPath = "root-CA.crt"
+certificatePath = "c5ffe0127c-certificate.pem.crt"
+privateKeyPath = "c5ffe0127c-private.pem.key"
+topic = "poll/tracking"
+
+myAWSIoTMQTTClient = AWSIoTMQTTClient("myClientID")
+myAWSIoTMQTTClient = AWSIoTMQTTClient("myClientID")
+myAWSIoTMQTTClient.configureEndpoint("a1jgcb96hr49vu-ats.iot.us-east-2.amazonaws.com", 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+myAWSIoTMQTTClient.connect()
+
 print("Connecting....")
 dev = btle.Peripheral("E2:B4:50:FA:1D:D9")
 for svc in dev.services: 
@@ -33,4 +56,6 @@ if __name__ == "__main__":
         humidity_int = converter(humidity)
         sensor_data = {"Temperature": temp_int, "Humidity": humidity_int}
         sensor_json = json.dumps(sensor_data)
-        print(sensor_json)
+        myAWSIoTMQTTClient.publish(topic, sensor_json, 1)
+        print('Published Topic %s: %s \n' % (topic, sensor_json))
+        sleep(1)
