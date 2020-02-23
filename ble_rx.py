b4dba7b93ca83a655fc581dc5d5e0c1fbd1829c6
Vasu77df <vasu3797@gmail.com>
2020-02-22 20:21:40 -0500

    intially working ble

--- ble_rx.py
+++ ble_rx.py
@@ -1,11 +1,11 @@
 from bluepy import btle
 print("Connecting....")
 dev = btle.Peripheral("E2:B4:50:FA:1D:D9")
+for svc in dev.services: 
+    print(str(svc))
 
-sensor = btle.UUID("0000180f-0000-1000-8000-00805f9b34fb")
-
-service = dev.getServiceByUUID(sensor)
-for ch in service.getCharacteristics():
+pollution_sensor = btle.UUID("190f")
+pollution_service = dev.getServiceByUUID(pollution_sensor)
+for ch in pollution_service.getCharacteristics():
     print(str(ch))
 
-    
