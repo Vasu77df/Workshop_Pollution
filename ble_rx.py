96519a40b362ae68455c0912694f9f1de509f064
Vasu77df <vasu3797@gmail.com>
2020-02-22 18:56:47 -0600

    ble rasp testing

--- ble_rx.py
+++ ble_rx.py
@@ -2,5 +2,10 @@ from bluepy import btle
 print("Connecting....")
 dev = btle.Peripheral("E2:B4:50:FA:1D:D9")
 
-for svc in dev.services:
-    print(svc)
+sensor = btle.UUID("00002a19-0000-1000-8000-00805f9b34fb")
+
+service = dev.getServiceByUUID(sensor)
+for ch in service.getCharacteristics():
+    print(str(ch))
+
+    
