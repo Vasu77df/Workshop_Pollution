191934c9a9b4513ad9ea136fa5838d703646392d
Vasu77df <vasu3797@gmail.com>
2020-02-23 08:48:57 -0600

    data handling

--- ble_rx.py
+++ ble_rx.py
@@ -17,14 +17,29 @@ temp_value = pollution_service.getCharacteristics(temperature_uuid)[0]
 humidity_value = pollution_service.getCharacteristics(humidity_uuid)[0]
 pressure_value = pollution_service.getCharacteristics(humidity_uuid)[0]
 
+def converter(data):
+    data = str(data)
+    data = data.strip('b')
+    data = data.strip("'")
+    data = data.strip('\\r\\n')
+    data = float(data)
 
+    retunr data
 
 # Read sensor
-while True:
-    temp = temp_value.read()
-    humidity = humidity_value.read()
-    pressure = pressure_value.read()
-    print(type(temp))
-    print(type(humidity))
-    sensor_data = {"Temperature": temp, "Humidity": humidity, "Pressure": pressure}
-    print(sensor_data)
+if __name__ == "__main__":
+    while True:
+        temp = temp_value.read()
+        humidity = humidity_value.read()
+        pressure = pressure_value.read()
+        print(type(temp))
+        print(type(humidity))
+        temp_int = converter(temp)
+        humidity_int = converter(humidity)
+        pressure_int = converter(pressure)
+        sensor_data = {"Temperature": temp, "Humidity": humidity, "Pressure": pressure}
+        print(sensor_data)
+        print(temp_int)
+        print(type(temp_int))
+        print(humidity_int)
+        print(pressure_int)
