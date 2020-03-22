5bf969c2369f0a4c96c890dd87ff9f7662a82f7a
Vasu77df <vasu3797@gmail.com>
2020-03-22 11:01:50 -0500

    for analytics

--- ble_rx.py
+++ ble_rx.py
@@ -57,7 +57,8 @@ if __name__ == "__main__":
         humidity_int = converter(humidity)
         milli = int(round(time.time()))
         current_time = time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(milli))
-        sensor_data = {"Sensor_time": milli, "Temperature": temp_int, "Humidity": humidity_int, "TimeStamp": current_time}
+        only_time = time.strftime("%H:%M:%S", time.localtime(milli))
+        sensor_data = {"Sensor_time": milli, "Temperature": temp_int, "Humidity": humidity_int, "TimeStamp": current_time, "Time": only_time}
         sensor_json = json.dumps(sensor_data)
         myAWSIoTMQTTClient.publish(topic, sensor_json, 1)
         print('Published Topic %s: %s \n' % (topic, sensor_json))
